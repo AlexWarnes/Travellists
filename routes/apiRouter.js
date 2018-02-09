8134020703e9e7eae09de910bb7418ad48133785
Alex Warnes <alex.s.warnes@gmail.com>
2018-02-09 09:19:08 -0500

    restored previous API router

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,60 +2,131 @@
 
 const express = require('express');
 const faker = require('faker');
-const router = express.Router(); 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+	mongoose.Promise = global.Promise;
 
-function makeFakeList() {
-	const list = {
-		city: faker.address.city(),
-		country: faker.address.country(),
-		title: faker.company.catchPhrase(),
-		description: faker.lorem.paragraph(),
-		places: [
-			{
-				placeName: 'Cafe ' + faker.lorem.word(),
-				placeLocation: [12345, 9876],
-				placeDescription: faker.lorem.sentence()
-			}, 
-			{
-				placeName: 'Cafe ' + faker.lorem.word(),
-				placeLocation: [12345, 9876],
-				placeDescription: faker.lorem.sentence()
-			}, 
-			{
-				placeName: 'Cafe ' + faker.lorem.word(),
-				placeLocation: [12345, 9876],
-				placeDescription: faker.lorem.sentence()
-			}
-		],
-		dateCreated: faker.date.recent()
-	};
-	return list;
-};
+const { List, User } = require('../models');
+
+const router = express.Router();
+router.use(bodyParser.json());
 
 
-//USED FOR CALLS TO /api
+//==================== GET REQUESTS ====================
 
 router.get('/lists', (req, res, next) => {
-	let fakerLists = [];
-	for (let i=0; i<5; i++) {
-		let list = makeFakeList();
-		fakerLists.push(list);
-	};
-	res.status(200).json(fakerLists);
+	List
+		.find()
+		.then(data => {
+			res.status(200)
+			.json(data.map((list) => list.serialize()));
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
 });
 
 router.get('/lists/:id', (req, res, next) => {
-	res.status(200).send('Add this route with test DB');
-})
+	List
+		.findById(req.params.id)
+		.then(list => {
+			res.status(200)
+			.json(list.serialize());
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
 
-router.get('/travelers', (req, res, next) => {
+router.get('/users', (req, res, next) => {
 	res.status(200).send('Feature set planned for Phase Two!');
 });
 
-router.get('/travelers/:id', (req, res, next) => {
+router.get('/users/:id', (req, res, next) => {
 	res.status(200).send('This might be how we display profiles to users');
 });
 
 
+//==================== POST REQUESTS ====================
+
+router.post('/lists', (req, res, next) => {
+	const requiredFields = ['city', 'country', 'title', 'description', 'places', 'author', 'dateCreated'];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+
+	List
+		.create({
+			city: req.body.city,
+			country: req.body.country,
+			title: req.body.title,
+			description: req.body.description,
+			places: req.body.places,
+			author: req.body.author,
+			dateCreated: req.body.dateCreated
+		})
+		.then(list => {
+			res.status(201)
+			.json(list.serialize());
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
+
+
+
+//==================== PUT REQUESTS ====================
+
+router.put('/lists/:id', (req, res, next) => {
+	if (req.params.id !== req.body.id) {
+		const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match.`;
+		console.error(message);
+		return res.status(400).json({message: message});
+	}
+
+	const toUpdate = {};
+	const updateableFields = ['city', 'country', 'title', 'description', 'places']
+
+	updateableFields.forEach(field => {
+		if (field in req.body) {	
+			toUpdate[field] = req.body[field];
+		}
+	});
+
+	List
+		.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+		.then(list => {
+			res.status(204).end()
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
+
+
+//==================== DELETE REQUESTS ====================
+
+router.delete('/lists/:id', (req, res, next) => {
+	List
+		.findByIdAndRemove(req.params.id)
+		.then(list => res.status(204).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
+
+router.use('*', (req, res, next) => {
+	res.status(404).json({message: 'Not Found'});
+});
 
 module.exports = router;
