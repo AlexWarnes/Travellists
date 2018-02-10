fe8a5832dd6b1a5dccc5f11904251e78703ba377
Alex Warnes <alex.s.warnes@gmail.com>
2018-02-09 22:03:35 -0500

    changed heroku app name; started building out client

--- public/Scripts/index.js
+++ public/Scripts/index.js
@@ -1,31 +1,34 @@
 'use strict'
 
-function viewLists() {
-	$('.viewListsButton').on('click', function(e) {
-		e.preventDefault();
-		console.log('Making some lists...');
-		getListData(displayLists);
-	});
+function dynamicSTORE() {
+	showLists();
+	viewThisList();
+	closeThisWindow();
 }
+
+//========== HOME SCREEN FUNCTIONS ==========
+
+function showLists() {
+		console.log('Getting some lists...');
+		getListData(displayLists);
+	};
 
 function renderLists(item) {
 	const numberOfPlaces = item.places.length;
 	return `
-		<a href="/lists/id">
-			<article class="list">
-				<p class="listLocation">${item.city}, ${item.country}</p>
-				<h4 class="listTitle">${item.title}</h4>
-				<p class="listDescription">${item.description}</p>
-				<p class="placesCount">${numberOfPlaces} places in this list</p>
-			</article>
-		</a>
+		<article class="listPreview" id="${item.id}">
+			<p class="listPreviewLocation">${item.city}, ${item.country}</p>
+			<h4 class="listPreviewTitle">${item.title}</h4>
+			<p class="listPreviewDescription">${item.description}</p>
+			<p class="listPreviewPlacesCount">${numberOfPlaces} places in this list</p>
+		</article>
 	`;
 }
 
 function displayLists(data) {
 	console.log(data);
 	const results = data.map((item) => renderLists(item));
-	$('.listsSection').html(results);
+	$('.listsGrid').html(results);
 }
 
 function getListData(callback) {
@@ -38,4 +41,63 @@ function getListData(callback) {
 	$.ajax(settings);
 }
 
-$(viewLists);
+//========== VIEW INDIVIDUAL LIST =========
+
+function viewThisList() {
+	$('.listsGrid').on('click', '.listPreview', function(e) {
+		const listId = this.id;
+		console.log(`Getting info for ${listId}`);
+		getThisListData(listId, displayThisList);
+	})
+}
+
+function displayThisList(data) {
+	console.log(data);
+	const listHtml = `
+		<div class="listIntro">
+			<h1 class="listLocation">${data.city}, ${data.country}</h1>
+			<h2 class="listTitle">${data.title}</h2>
+			<p class="listDescription">${data.description}</p>
+		</div>`;
+	
+	data.places.forEach((item) => {
+		let place = `
+			<li class="place">
+				<h3 class="placeName">${item.placeName}</h3>
+				<p class="placeDescription">${item.placeDescription}</p>
+			</li>`;
+		$('.singleList').html(listHtml);
+		$('.listPlaces').append(place);
+	});
+	$('.listView').toggle(1000);
+}
+
+function getThisListData(id, callback) {
+	const settings = {
+		url: `/api/lists/${id}`,
+		dataType: 'JSON',
+		method: 'GET',
+		success: callback
+	};
+	$.ajax(settings);
+}
+
+function closeThisWindow() {
+	$('.close').on('click', function(e) {
+		e.preventDefault();
+		$(this).parent().toggle(1000);
+	$('.singleList').html('');
+	$('.listPlaces').html('');	
+	})
+}
+
+
+
+
+
+
+
+
+
+
+$(dynamicSTORE);
