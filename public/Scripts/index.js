0373241cd65a83f74d5d263eafc32f6dd6b17688
Alex Warnes <alex.s.warnes@gmail.com>
2018-02-24 16:10:27 -0500

    completed PUT functionality to update or edit lists

--- public/Scripts/index.js
+++ public/Scripts/index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 function switchView(currentView, nextView) {
-	currentView.slideToggle(500);
-	nextView.slideToggle(500);
+	currentView.slideToggle({
+		duration: 500, 
+		complete: nextView.slideToggle(500)
+	});
 }
 
 function STARTUP() {
@@ -14,6 +16,7 @@ function STARTUP() {
 	addAnotherPlace();
 	submitList();
 	deleteThisList();
+	updateList();
 	editList();
 	cancelEditList();
 }
@@ -111,7 +114,8 @@ function getThisListData(id, callback) {
 function xListWindow() {
 	$('.close').on('click', function(e) {
 		e.preventDefault();
-		switchView($('.listView'), $('.gridView'))		
+		switchView($('.listView'), $('.gridView'))
+		showLists();	
 		clearListInfo();
 	})
 }
@@ -195,7 +199,7 @@ function submitList() {
 }
 
 function successfulPost() {
-	switchView($('fieldset'), $('.gridView'));
+	switchView($('.newListFieldset'), $('.gridView'));
 	showLists();
 	resetListForm();
 }
@@ -267,23 +271,99 @@ function editList() {
 		console.log(`Getting info for ${listId}`);
 		switchView($('.listView'), $('.editListFieldset'));
 		getThisListData(listId, populateEditFields);
-	})
+	});
 }
 
 function populateEditFields(data) {
+	$('.editListForm').attr('id', data.id);
 	$('#editListCity').val(`${data.city}`);
 	$('#editListCountry').val(`${data.country}`);
 	$('#editListTitle').val(`${data.title}`);
 	$('#editListDescription').val(`${data.description}`);
-	//Loop over data.places and add fields for editing
+	renderPlaces(data);
+}
+
+function renderPlaces(data) {
+	const numberOfPlaces = data.places.length;
+	for (let i = 0; i < numberOfPlaces; i++) {
+		// console.log(data.places[i]);
+		$('.editListPlaces').append(`
+			<li class="editListPlace">
+				<label for="editPlaceName-${i}">Place Name</label>
+				<input type="text" value="${data.places[i].placeName}" name="placeName" id="editPlaceName-${i}" class="editListPlaceName listInput">
+
+				<label for="editPlaceDescription-${i}">Place Description</label>
+				<input type="text" value="${data.places[i].placeDescription}" name="placeDescription" id="editPlaceDescription-${i}" class="editListPlaceDescription listInput">
+			</li>
+		`);
+	}
+}
+
+function updateList() {
+	$('.updateListButton').on('click', function(e) {
+		e.preventDefault();
+		const arrayOfPlaces = renderUpdatedListPlaces();
+		const listId = $('.editListForm').attr('id');
+		
+		const updatedList = {
+			// author: req.user,
+			id: listId,
+			city: $('#editListCity').val(),
+			country: $('#editListCountry').val(),
+			title: $('#editListTitle').val(),
+			description: $('#editListDescription').val(),
+			places: arrayOfPlaces
+		};
+		
+		const updatedListJson = JSON.stringify(updatedList);
+		
+		const settings = {
+			url: `/api/lists/${listId}`,
+			data: updatedListJson,
+			contentType: 'application/json',
+			type: 'PUT',
+			success: successfulUpdate
+		}
+		console.log(updatedListJson);
+		$.ajax(settings);
+	});
+}
+
+function renderUpdatedListPlaces() {
+	const arrayOfPlaces = [];
+	
+	for (let i = 0; i < $('.editListPlace').length; i++) {
+		const currentPlaceName = `editPlaceName-${i}`;
+		const currentPlaceDescription = `editPlaceDescription-${i}`;
+
+		arrayOfPlaces[i] = {
+			placeName: $('#' + currentPlaceName).val(),
+			placeDescription: $('#' + currentPlaceDescription).val()
+		}
+	}
+	return arrayOfPlaces;
+}
+
+function successfulUpdate() {
+	switchView($('.editListFieldset'), $('.listView'));
+	const listId = $('.listIntro').attr('id');
+	clearListInfo();
+	getThisListData(listId, displayThisList);
+	resetEditListForm();
+}
+
+function resetEditListForm() {
+	$('.editListPlaces').html('');
+	$('.listInput').val('');
+	$('.editListForm').attr('id');
 }
 
 function cancelEditList() {
 	$('.cancelEditButton').on('click', function(e) {
 		e.preventDefault();
 		switchView($('.editListFieldset'), $('.listView'));
-		resetListForm();
-	})
+		resetEditListForm();
+	});
 }
 
 $(STARTUP);
